8631582f66820c0f14ce2f502c6dc4ebf4c1ac2d
mskang <remote.mskang@gmail.com>
2023-08-29 13:39:43 +0900

    db setting change

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,4 @@
+import environ
 from .base import *
 
 ALLOWED_HOSTS = ['3.36.116.191']
@@ -5,7 +6,19 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
 
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
 
 
 
